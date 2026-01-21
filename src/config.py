25fd341635f2d2a29f8e67a56ab701a41b695422
Sobikaa-droid <olegbrain0@gmail.com>
2026-01-21 20:42:16 +0500

    Added .env.example

--- src/config.py
+++ src/config.py
@@ -9,8 +9,8 @@ BASE_DIR = Path(__file__).parent.parent
 load_dotenv()
 
 class AuthJWT(BaseModel):
-    PRIVATE_KEY_PATH: Path = Path(os.getenv("PRIVATE_KEY_PATH", "/certs/private.pem"))
-    PUBLIC_KEY_PATH: Path = Path(os.getenv("PUBLIC_KEY_PATH", "/certs/public.pem"))
+    PRIVATE_KEY_PATH: Path = Path(os.getenv("PRIVATE_KEY_PATH"))
+    PUBLIC_KEY_PATH: Path = Path(os.getenv("PUBLIC_KEY_PATH"))
     algorithm: str = "RS256"
     access_token_expire_minutes: int = 15
     refresh_token_expire_days: int = 7
